ad596b07e79e745181b03dca136594da82c65461
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2025-04-01 18:38:16 -0600

    reset proyect

--- App.js
+++ App.js
@@ -1,32 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Squares } from './testComponents/heightWidth';
-
+import React from 'react';
 const App = () => {
   return (
-    <View style= {styles.container}>
-      <Text>fchDev.io
-      </Text>
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    height: 550,
-    backgroundColor: 'red'
-  },
-  header: {
-    flex:1,
-    width: 200,
-    height: 50,
-    backgroundColor: 'blue'
-  },
-  smallSquare: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'powderblue'
-  }
-})
 export default App;
+  
